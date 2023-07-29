1881a20e599d401c0f5d971e1975c41a03bf8e86
ugackMiner <ugackMiner@gmail.com>
2023-07-29 18:15:26 -0400

    Updates

---
+++ assets/tilemaps/cave/cave_bg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="cave_bg" tilewidth="32" tileheight="32" tilecount="144" columns="16">
+ <image source="back-walls.png" width="512" height="288"/>
+</tileset>
